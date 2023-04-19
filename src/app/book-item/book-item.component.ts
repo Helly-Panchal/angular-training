442a203837ff853a7eb4732495ba7692b7d12538
Helly-Panchal <helly.panchal@bacancy.com>
2023-04-19 16:43:11 +0530

    Modified files from CSS to SCSS

--- src/app/book-item/book-item.component.ts
+++ src/app/book-item/book-item.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-book-item',
   templateUrl: './book-item.component.html',
-  styleUrls: ['./book-item.component.css']
+  styleUrls: ['./book-item.component.scss']
 })
 export class BookItemComponent {
   imageUrl = ['../../assets/Images/cardOne.jpg', '../../assets/Images/cardTwo.jpg','../../assets/Images/cardThree.jpg','../../assets/Images/cardFour.jpg','../../assets/Images/cardFive.jpg','../../assets/Images/cardSix.jpg'];
--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.css']
+  styleUrls: ['./book-list.component.scss']
 })
 export class BookListComponent {
 
