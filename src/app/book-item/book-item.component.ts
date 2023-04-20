aeaf14e2f2524afdc8956537045ac7a0b7f3b031
Helly-Panchal <helly.panchal@bacancy.com>
2023-04-20 19:55:47 +0530

    Added data dynamically into cards and sidebar section using interpolation

--- src/app/book-item/book-item.component.ts
+++ src/app/book-item/book-item.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class BookItemComponent {
   imageUrl = ['../../assets/Images/cardOne.jpg', '../../assets/Images/cardTwo.jpg','../../assets/Images/cardThree.jpg','../../assets/Images/cardFour.jpg','../../assets/Images/cardFive.jpg','../../assets/Images/cardSix.jpg'];
+  cardDetailsObject = [
+  {
+    title: "Card title - 1",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, illo qui officia non sed illum velit quis! Sapiente tempora quisquam ipsa reiciendis unde quam quibusdam? Explicabo impedit voluptatibus qui ab?voluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui ab"
+  },
+  {
+    title: "Card title - 2",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, illo qui officia non sed illum velit quis! Sapiente tempora quisquam ipsa reiciendis unde quam quibusdam? Explicabo impedit voluptatibus qui ab?voluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui ab"
+  },
+  {
+    title: "Card title - 3",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, illo qui officia non sed illum velit quis! Sapiente tempora quisquam ipsa reiciendis unde quam quibusdam? Explicabo impedit voluptatibus qui ab?voluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui ab"
+  },
+  {
+    title: "Card title - 4",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, illo qui officia non sed illum velit quis! Sapiente tempora quisquam ipsa reiciendis unde quam quibusdam? Explicabo impedit voluptatibus qui ab?voluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui ab"
+  },
+  {
+    title: "Card title - 5",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, illo qui officia non sed illum velit quis! Sapiente tempora quisquam ipsa reiciendis unde quam quibusdam? Explicabo impedit voluptatibus qui ab?voluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui ab"
+  },
+  {
+    title: "Card title - 6",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, illo qui officia non sed illum velit quis! Sapiente tempora quisquam ipsa reiciendis unde quam quibusdam? Explicabo impedit voluptatibus qui ab?voluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui ab"
+  }
+];
+buttonTitile = "Visit";
 }
