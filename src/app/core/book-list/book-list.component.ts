79d3aa89497469badebb7c5ba426fa0c59984ba9
Helly-Panchal <helly.panchal@bacancy.com>
2023-04-24 20:33:22 +0530

    Added @Input decorator

--- src/app/core/book-list/book-list.component.ts
+++ src/app/core/book-list/book-list.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
 
+//interface
+import { ICardDetails } from '../interfaces/book-item.interface';
+
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.scss']
+  styleUrls: ['./book-list.component.scss'],
 })
 export class BookListComponent {
-
+  public cardDetailsObject: ICardDetails[] = [
+    {
+      title: 'Card title - 1',
+      imageUrl: '../../assets/Images/cardOne.jpg',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, illo qui officia non sed illum velit quis! Sapiente tempora quisquam ipsa reiciendis unde quam quibusdam? Explicabo impedit voluptatibus qui ab?voluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui ab',
+      buttonTitile: 'Visit',
+    },
+    {
+      title: 'Card title - 2',
+      imageUrl: '../../assets/Images/cardTwo.jpg',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, illo qui officia non sed illum velit quis! Sapiente tempora quisquam ipsa reiciendis unde quam quibusdam? Explicabo impedit voluptatibus qui ab?voluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui ab',
+      buttonTitile: 'Visit',
+    },
+    {
+      title: 'Card title - 3',
+      imageUrl: '../../assets/Images/cardThree.jpg',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, illo qui officia non sed illum velit quis! Sapiente tempora quisquam ipsa reiciendis unde quam quibusdam? Explicabo impedit voluptatibus qui ab?voluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui ab',
+      buttonTitile: 'Visit',
+    },
+    {
+      title: 'Card title - 4',
+      imageUrl: '../../assets/Images/cardFour.jpg',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, illo qui officia non sed illum velit quis! Sapiente tempora quisquam ipsa reiciendis unde quam quibusdam? Explicabo impedit voluptatibus qui ab?voluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui ab',
+      buttonTitile: 'Visit',
+    },
+    {
+      title: 'Card title - 5',
+      imageUrl: '../../assets/Images/cardFive.jpg',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, illo qui officia non sed illum velit quis! Sapiente tempora quisquam ipsa reiciendis unde quam quibusdam? Explicabo impedit voluptatibus qui ab?voluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui ab',
+      buttonTitile: 'Visit',
+    },
+    {
+      title: 'Card title - 6',
+      imageUrl: '../../assets/Images/cardSix.jpg',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, illo qui officia non sed illum velit quis! Sapiente tempora quisquam ipsa reiciendis unde quam quibusdam? Explicabo impedit voluptatibus qui ab?voluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui abvoluptatibus qui ab',
+      buttonTitile: 'Visit',
+    },
+  ];
 }
