b07dda3287b1f84e78642daf3f1d63869bc742e7
Helly-Panchal <helly.panchal@bacancy.com>
2023-05-04 11:48:01 +0530

    Call route from TS file (Programatically)

--- src/app/routing-demo/account/account.component.ts
+++ src/app/routing-demo/account/account.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
 })
 export class AccountComponent {
-
+  public accountsList: string[] = [
+    'Account - 1',
+    'Account - 2',
+    'Account - 3',
+    'Account - 4',
+    'Account - 5',
+    'Account - 6',
+  ];
 }
--- src/app/routing-demo/products/products.component.ts
+++ src/app/routing-demo/products/products.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
 
+  public goToHome(): void {
+    this.router.navigate(['/']);
+  }
+  public goToAccounts(): void {
+    // this.router.navigate(['accounts'], { relativeTo: this.activatedRoute });
+    this.router.navigate(['/accounts']);
+  }
 }
