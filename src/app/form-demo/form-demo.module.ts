92a8b96824fe0e3863305298278e5753f63b19b6
Helly-Panchal <helly.panchal@bacancy.com>
2023-05-07 14:20:24 +0530

    Submitted form by using ngForm

--- src/app/form-demo/form-demo.module.ts
+++ src/app/form-demo/form-demo.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormComponent } from './form/form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [FormComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [FormComponent],
 })
 export class FormDemoModule {}
--- src/app/form-demo/form/form.component.ts
+++ src/app/form-demo/form/form.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent {
-
+  public submit(form: NgForm) {
+    console.log(form);
+  }
 }
