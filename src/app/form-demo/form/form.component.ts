6588a3ca26c29f1cd5d6edf0357beef4384c55f9
Helly-Panchal <helly.panchal@bacancy.com>
2023-05-09 10:12:46 +0530

    Displayed entered form data and implemented reset form functionality

--- src/app/form-demo/form/form.component.ts
+++ src/app/form-demo/form/form.component.ts
@@ -9,6 +9,16 @@ import { NgForm } from '@angular/forms';
 export class FormComponent {
   public defaultLocation: string = 'AHD';
   public genders = ['Male', 'Female'];
+  public isSubmitted: boolean = false;
+  public user = {
+    firstName: '',
+    lastName: '',
+    emailId: '',
+    password: '',
+    location: '',
+    gender: '',
+    rememberMe: false,
+  };
 
   @ViewChild('form') firstTemplateDrivenForm!: NgForm;
 
@@ -18,9 +28,19 @@ export class FormComponent {
 
   public onSubmit(): void {
     console.log(this.firstTemplateDrivenForm);
+    this.isSubmitted = true;
+    this.user.firstName = this.firstTemplateDrivenForm.value.firstName;
+    this.user.lastName = this.firstTemplateDrivenForm.value.lastName;
+    this.user.emailId = this.firstTemplateDrivenForm.value.email;
+    this.user.password = this.firstTemplateDrivenForm.value.pwd;
+    this.user.gender = this.firstTemplateDrivenForm.value.gender;
+    this.user.location = this.firstTemplateDrivenForm.value.location;
+    this.user.rememberMe = this.firstTemplateDrivenForm.value.remember;
+
+    this.firstTemplateDrivenForm.reset();
   }
 
-  setValueMethod() {
+  public setValueMethod(): void {
     this.firstTemplateDrivenForm.form.setValue({
       firstName: 'John',
       lastName: 'Doe',
@@ -32,7 +52,7 @@ export class FormComponent {
     });
   }
 
-  patchValueMethod() {
+  public patchValueMethod(): void {
     this.firstTemplateDrivenForm.form.patchValue({
       location: 'RJT',
       gender: 'Female',
