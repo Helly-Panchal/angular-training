35922eb3eff6c32a0947a41a8b067cef22aec971
Helly-Panchal <helly.panchal@bacancy.com>
2023-05-12 12:19:10 +0530

    Created pipe-demo module

---
+++ src/app/pipe-demo/pipe-demo.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PipeExampleComponent } from './pipe-example/pipe-example.component';
+
+
+
+@NgModule({
+  declarations: [
+    PipeExampleComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PipeDemoModule { }
---
+++ src/app/pipe-demo/pipe-example/pipe-example.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipe-example',
+  templateUrl: './pipe-example.component.html',
+  styleUrls: ['./pipe-example.component.css']
+})
+export class PipeExampleComponent {
+
+}
