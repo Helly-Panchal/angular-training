cfa701878721c7ae5c5de739bef12ba9e2402d3b
Helly-Panchal <helly.panchal@bacancy.com>
2023-05-18 10:45:11 +0530

    Implemented behavior subject

--- src/app/subject-demo/subject/subject.component.ts
+++ src/app/subject-demo/subject/subject.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-subject',
@@ -10,14 +10,20 @@ export class SubjectComponent implements OnInit {
   ngOnInit(): void {
     // observable
     const observable = new Observable(obj => obj.next(Math.random()));
-    observable.subscribe(res => console.log("Response from observable : ", res));
+    observable.subscribe(res => console.log("\n\nResponse from observable : ", res));
     observable.subscribe(res => console.log("Response from observable : ", res));
 
     // subject
     const subject = new Subject();
+    subject.subscribe(res => console.log("\n\nResponse from subject : ", res));
     subject.subscribe(res => console.log("Response from subject : ", res));
-    subject.subscribe(res => console.log("Response from subject : ", res));
-
     subject.next(Math.random());
+
+    // behavior subject
+    const behaviorSubject = new BehaviorSubject("Default sentence from behavior subject..!!");
+    behaviorSubject.next("Hello again from behavior subject.");
+    behaviorSubject.next("Hello again from behavior subject-2.");
+    behaviorSubject.subscribe(res => console.log("\n\nResponse from behavior subject : ", res));
+    behaviorSubject.next("Hello again from behavior subject-3.");
   }
 }
